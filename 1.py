8dd5a56c26c8926bcb139d7d558eb33fa31ab765
OlesyaTulinova <olesya_tulinova_04@mail.ru>
2020-02-08 14:04:20 +0300

    №3

--- 1.py
+++ 1.py
@@ -4,9 +4,9 @@ import pygame
 import requests
 
 response = None
-coords = input("Введите координаты в формате 'долгота,широта':  ")
-spn = input("Введите масштаб через ',' без пробелов:  ")
-map_request = f'https://static-maps.yandex.ru/1.x/?ll={coords}&spn={spn}&l=sat'
+coords = ''.join(input("Введите координаты в формате 'долгота,широта':  ").split()).split(',')
+spn = ''.join(input("Введите масштаб через ',' без пробелов:  ").split()).split(',')
+map_request = f'https://static-maps.yandex.ru/1.x/?ll={",".join(coords)}&spn={",".join(spn)}&l=sat'
 response = requests.get(map_request)
 
 if not response:
@@ -27,8 +27,30 @@ screen = pygame.display.set_mode((600, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
 # Переключаем экран и ждем закрытия окна.
 pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            response = None
+            if event.key == 273:  # up
+                coords[1] = str(float(coords[1]) + float(spn[1]))
+            elif event.key == 274:  # down
+                coords[1] = str(float(coords[1]) - float(spn[1]))
+            elif event.key == 275:  # right
+                coords[0] = str(float(coords[0]) + float(spn[0]))
+            elif event.key == 276:  # left
+                coords[0] = str(float(coords[0]) - float(spn[0]))
+            map_request = f'https://static-maps.yandex.ru/1.x/?ll={",".join(coords)}&spn={",".join(spn)}&l=sat'
+            response = requests.get(map_request)
+
+            if response:
+                map_file = "map.png"
+                with open(map_file, "wb") as file:
+                    file.write(response.content)
+                screen.blit(pygame.image.load(map_file), (0, 0))
+                pygame.display.flip()
 pygame.quit()
 
 # Удаляем за собой файл с изображением.
