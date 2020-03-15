1c3c0ef08fb8aee4638f798a7044c426b74713fd
GrishHelen <grishina-helen@rambler.ru>
2020-03-15 20:58:50 +0300

    №5 v2.0

--- 1.py
+++ 1.py
@@ -47,49 +47,48 @@ class MyWidget(QMainWindow, QWidget):
         self.text = self.line.text()
 
 
-response = None
+map_file = "map.png"
 coords = list(map(float, input("Введите координаты в формате 'долгота,широта':  ").split(",")))
-spn = float(input("Введите масштаб  ").strip())
+z = int(input("Введите масштаб от 0 до 17, где 0 - это весь земной шар:  "))
 
-# Инициализируем pygame
 pygame.init()
-screen = pygame.display.set_mode((600, 450))
+screen = pygame.display.set_mode((450, 450))
+
 ex = ''
 q = 'sat'
 f1 = pygame.font.Font(None, 25)
 text1 = f1.render('Спутник', 1, (180, 0, 0))
 text2 = f1.render('Карта', 1, (0, 180, 0))
 text3 = f1.render('Гибрид', 1, (0, 180, 0))
-screen.blit(text1, (0, 5))
-screen.blit(text2, (70, 5))
-screen.blit(text3, (125, 5))
 text4 = f1.render('Найти', 1, (250, 250, 0), (0, 0, 0))
-screen.blit(text4, (0, 30))
 
-# Рисуем картинку, загружаемую из только что созданного файла.
 running = True
+change = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                coords[1] += spn
+                coords[1] = (coords[1] + 180.0 / (2 ** z) + 90) % 180 - 90
             elif event.key == pygame.K_DOWN:
-                coords[1] -= spn
+                coords[1] = (coords[1] - 180.0 / (2 ** z) + 90) % 180 - 90
             elif event.key == pygame.K_RIGHT:
-                coords[0] += spn
+                coords[0] = (coords[0] + 360.0 / (2 ** z) + 180) % 360 - 180
             elif event.key == pygame.K_LEFT:
-                coords[0] -= spn
-            elif event.key == pygame.K_PAGEDOWN:
-                if spn - 5 >= 0:
-                    spn -= 5
+                coords[0] = (coords[0] - 360.0 / (2 ** z) + 180) % 360 - 180
+            if event.key == pygame.K_PAGEDOWN:
+                # у меня на компе на этих кнопках написано PgUp и PgDn, но pygame их неправильно определяет
+                if z > 0:
+                    z -= 1
             elif event.key == pygame.K_PAGEUP:
-                if spn + 5 < 95:
-                    spn += 5
+                if z < 17:
+                    z += 1
+            change = True
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             pos = event.pos
+            # может лучше делать через спрайты? Нет так нет, как хочешь
             if pos[0] <= 65 and pos[1] <= 23:
                 q = 'sat'
                 text1 = f1.render('Спутник', 1, (180, 0, 0))
@@ -109,36 +108,42 @@ while running:
                 app = QApplication(sys.argv)
                 ex = MyWidget()
                 ex.show()
+            change = True
+
     if ex != '' and ex.is_clicked:
         find = ex.text
         ex.close()
         ex = ''
         form_map_request(find)
+        change = True
 
-    map_request = f'https://static-maps.yandex.ru/1.x/?ll=' \
-                  f'{",".join(map(str, coords))}&' \
-                  f'spn={",".join([str(spn)] * 2)}&l={q}'
-    response = requests.get(map_request)
-    if not response:
-        print()
-        # print(spn)
-        print("Ошибка выполнения запроса:")
-        print(map_request)
-        print("Http статус:", response.status_code, "(", response.reason, ")")
-    else:
-        # Запишем полученное изображение в файл.
-        map_file = "map.png"
-        with open(map_file, "wb") as file:
-            file.write(response.content)
-        screen.fill((0, 0, 0))
-        screen.blit(pygame.image.load(map_file), (0, 0))
-        screen.blit(text1, (0, 5))
-        screen.blit(text2, (70, 5))
-        screen.blit(text3, (125, 5))
-        screen.blit(text4, (0, 30))
-        pygame.display.flip()
+    if change:
+        map_api_server = 'https://static-maps.yandex.ru/1.x/'
+        params = {
+            "l": q,
+            "z": z,
+            "ll": ','.join(map(str, coords)),
+            "size": "450,450",
+        }
+        response = requests.get(map_api_server, params=params)
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(params)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        else:
+            with open(map_file, "wb") as file:
+                file.write(response.content)
+
+            screen.blit(pygame.image.load(map_file), (0, 0))
+            screen.blit(text1, (0, 5))
+            screen.blit(text2, (70, 5))
+            screen.blit(text3, (125, 5))
+            screen.blit(text4, (0, 30))
+            pygame.display.flip()
+
+    change = False
 
 pygame.quit()
-
-# Удаляем за собой файл с изображением.
 os.remove(map_file)
