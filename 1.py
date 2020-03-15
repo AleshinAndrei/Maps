43ae8ad73da6e1f39745dc9b2e2bcfc29ddb929d
GrishHelen <grishina-helen@rambler.ru>
2020-03-15 19:31:05 +0300

    №5

--- 1.py
+++ 1.py
@@ -2,6 +2,50 @@ import os
 import sys
 import pygame
 import requests
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+
+
+def form_map_request(find):
+    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&" \
+                       f"geocode={find}&format=json"
+    # Выполняем запрос.
+    response = requests.get(geocoder_request)
+    if response:
+        # Преобразуем ответ в json-объект
+        json_response = response.json()
+
+        # Получаем первый топоним из ответа геокодера.
+        # Согласно описанию ответа, он находится по следующему пути:
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        # Полный адрес топонима:
+        toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+        # Координаты центра топонима:
+        toponym_coodrinates = toponym["Point"]["pos"]
+
+        # Печатаем извлечённые из ответа поля:
+        coord = toponym_coodrinates.split(' ')
+        coord = list(map(float, coord))
+        global coords
+        coords = coord
+    else:
+        print("Ошибка выполнения запроса:")
+        print(geocoder_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+
+
+class MyWidget(QMainWindow, QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('search.ui', self)
+        self.text = ''
+        self.is_clicked = False
+        self.btn_search.clicked.connect(self.search)
+
+    def search(self):
+        self.is_clicked = True
+        self.text = self.line.text()
+
 
 response = None
 coords = list(map(float, input("Введите координаты в формате 'долгота,широта':  ").split(",")))
@@ -10,15 +54,17 @@ spn = float(input("Введите масштаб  ").strip())
 # Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-
+ex = ''
 q = 'sat'
 f1 = pygame.font.Font(None, 25)
 text1 = f1.render('Спутник', 1, (180, 0, 0))
 text2 = f1.render('Карта', 1, (0, 180, 0))
 text3 = f1.render('Гибрид', 1, (0, 180, 0))
 screen.blit(text1, (0, 5))
-screen.blit(text2, (0, 25))
-screen.blit(text3, (0, 45))
+screen.blit(text2, (70, 5))
+screen.blit(text3, (125, 5))
+text4 = f1.render('Найти', 1, (250, 250, 0), (0, 0, 0))
+screen.blit(text4, (0, 30))
 
 # Рисуем картинку, загружаемую из только что созданного файла.
 running = True
@@ -41,7 +87,7 @@ while running:
             elif event.key == pygame.K_PAGEUP:
                 if spn + 5 < 95:
                     spn += 5
-        
+
         elif event.type == pygame.MOUSEBUTTONDOWN:
             pos = event.pos
             if pos[0] <= 65 and pos[1] <= 23:
@@ -59,6 +105,15 @@ while running:
                 text1 = f1.render('Спутник', 1, (0, 180, 0))
                 text2 = f1.render('Карта', 1, (0, 180, 0))
                 text3 = f1.render('Гибрид', 1, (180, 0, 0))
+            elif pos[0] <= 50 and 30 <= pos[1] <= 50:
+                app = QApplication(sys.argv)
+                ex = MyWidget()
+                ex.show()
+    if ex != '' and ex.is_clicked:
+        find = ex.text
+        ex.close()
+        ex = ''
+        form_map_request(find)
 
     map_request = f'https://static-maps.yandex.ru/1.x/?ll=' \
                   f'{",".join(map(str, coords))}&' \
@@ -66,7 +121,7 @@ while running:
     response = requests.get(map_request)
     if not response:
         print()
-        print(spn)
+        # print(spn)
         print("Ошибка выполнения запроса:")
         print(map_request)
         print("Http статус:", response.status_code, "(", response.reason, ")")
@@ -80,6 +135,7 @@ while running:
         screen.blit(text1, (0, 5))
         screen.blit(text2, (70, 5))
         screen.blit(text3, (125, 5))
+        screen.blit(text4, (0, 30))
         pygame.display.flip()
 
 pygame.quit()
